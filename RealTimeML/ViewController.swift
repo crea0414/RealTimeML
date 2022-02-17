4063a58089390552d4b50d241559d90d9bda8f31
creacyhuang <creacyhuang@gmail.com>
2022-02-17 21:03:48 +0800

    Initial Commit

---
+++ RealTimeML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeML
+//
+//  Created by 吳佳穎 on 2022/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
